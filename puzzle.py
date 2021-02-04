79ae516b15c3458cd4239bdce8be527427c8898d
Bohdan Shevchuk <shevdan007@gmail.com>
2021-02-05 01:04:33 +0200

    Implemented one cell check and row check

---
+++ puzzle.py
@@ -0,0 +1,52 @@
+'''
+This module is designed to check if the game board is ready to play
+
+'''
+
+EXAMPLE = [
+ "**** ****",
+ "***1 ****",
+ "**  3****",
+ "* 4 1****",
+ "     9 5 ",
+ " 6  83  *",
+ "3   1  **",
+ "  8  2***",
+ "  2  ****"
+]
+
+
+def check_cell(symb: str, used: list) -> bool:
+    '''
+    Check if the symbol in the cell was already used.
+    Returns True if symbol is ' ', '*' or not in used
+    and False otherwise
+
+    '''
+    if symb in [' ', '*']:
+        return True
+
+    if symb not in used:
+        used.append(symb)
+        return True
+
+    return False
+
+def check_row(board: list, row: int) -> bool:
+    '''
+    Check if the symbols in a given row are unique or ' ', '*'
+    '''
+
+    used = []
+    for col in range(len(board)):
+        if not check_cell(board[row][col], used):
+            return False
+    return True
+
+
+
+
+
+if __name__ == "__main__":
+    print(check_row(EXAMPLE, 2))
+
