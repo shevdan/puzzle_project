a724d36adad24d975a3f51ab57cf417c253dbe58
Bohdan Shevchuk <shevdan007@gmail.com>
2021-02-05 17:06:53 +0200

    Updated check_cell and added check_color function for checking the colored corner

--- puzzle.py
+++ puzzle.py
@@ -4,7 +4,7 @@ This module is designed to check if the game board is ready to play
 '''
 
 EXAMPLE = [
- "**** ****",
+"**** ****",
  "***1 ****",
  "**  3****",
  "* 4 1****",
@@ -20,13 +20,13 @@ def check_cell(symb: str, used: list) -> bool:
     '''
     Check if the symbol in the cell was already used.
     Returns True if symbol is ' ', '*' or not in used
-    and False otherwise
+    and False otherwise. ' ' and '*' are included in visited
+    by default so they do not affect the function.
 
     '''
-    if symb in [' ', '*']:
-        return True
 
-    if symb not in used:
+
+    if symb not in used or symb in (' ', '*'):
         used.append(symb)
         return True
 
@@ -53,14 +53,39 @@ def check_col(board:list, col: int) -> bool:
 
     used = []
     for row in range(len(board)):
-        print(used)
         if not check_cell(board[row][col], used):
             return False
 
     return True
 
+def check_color(board:list) -> bool:
+    '''
+    Check if the symbols in a colored corner 5x5 are unique or ' ' or '*'.
+    Colors indexing starts at 0 meaning the leftmost corner of the border.
+    If any of the symbols in any of the colors is not unique, returns False
+
+    '''
+
+    #board should be at least 5x5 size to contain colored colors
+    #max number of colors is size of the board - 4
+    num_col = len(board) - 4
+    for curr_col in range(num_col):
+        used = []
+        color_idx = len(board) - curr_col - 1
+        corner = board[color_idx][curr_col]
+        used.append(corner)
+        for idx in range(1, 5):
+            #check the colored 5 cells in a row, excluding corner cell
+            check_idx = curr_col + idx
+            if not check_cell(board[color_idx][check_idx], used):
+                return False
+            #check the colored 5 cells in a column, excluding corner cell
+            #len(board) - check_idx reverses the index for column indexation
+            if not check_cell(board[color_idx - idx][curr_col], used):
+                return False
+        return True
 
 
 if __name__ == "__main__":
-    print(check_col(EXAMPLE, 4))
+    print(check_color(EXAMPLE))
 
